c3c651e072d5127b3f7b18b0944ee34aeb4c269b
mark.jang <>
2020-08-27 21:52:01 +0900

    add application context

--- app/src/main/java/io/hwjang/app/googlebooks/di/module/AppModule.kt
+++ app/src/main/java/io/hwjang/app/googlebooks/di/module/AppModule.kt
@@ -1,12 +1,16 @@
 package io.hwjang.app.googlebooks.di.module
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 @InstallIn(ApplicationComponent::class)
 @Module
 class AppModule {
 
+    @Provides
+    fun provideApplicationContext(context: ApplicationContext) = context
 
 }
